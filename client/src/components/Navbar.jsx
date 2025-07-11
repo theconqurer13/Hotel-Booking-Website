e69557443b38414e6720c92a2e9fd3b55f79ca1c
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-07-11 23:06:34 +0530

    completed the Allrooms page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -60,7 +60,7 @@ useEffect(() => {
 
                     {user ? (<button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`} onClick={()=>{
                         navigate('/owner')
-                    }}>
+                    }}> Owner
                      </button>) :
                     (<button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}>
                         Dashboard
@@ -99,13 +99,13 @@ useEffect(() => {
                             <UserButton.Action label = "My Bookings" labelIcon={<BookIcon/>} onClick={()=> navigate('/my-bookings')}/>
                         </UserButton.MenuItems>
                       </UserButton>}
-                    <img onClick={()=>setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="" className={`${isScrolled && 'invert' } h-4`}/>
+                    <img onClick={()=>setIsMenuOpen(!isMenuOpen)} src={assets.menuIcon} alt="" className={`${isScrolled && 'invert' } h-4 cursor-pointer`}/>
                 </div>
 
                 {/* Mobile Menu */}
                 <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
-                       <img src={assets.closeIcon} alt="close menu" className='h-6.5'/>
+                       <img src={assets.closeIcon} alt="close menu" className='h-6.5 cursor-pointer'/>
                     </button>
 
                     {navLinks.map((link, i) => (
